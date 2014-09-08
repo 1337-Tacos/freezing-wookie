ad5c40b50cd4d8f94657e2ac278a5edadeab410c
1n5aN1aC <joshuavillwo@gmail.com>
2014-09-07 19:00:20 -0700

    Finishing up updateList()
    This also moves all parsing into it's own class, as it was getting long.
    Still some debug code in there, but I don't feel comfortable removing
    that yet.

---
+++ src/core/RepoParser.java
@@ -0,0 +1,143 @@
+package core;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Vector;
+
+public abstract class RepoParser {
+
+	/**
+	 * Downloads the document for the repository, and then re-assembles the document, re-writing the line endings.
+	 * This should prevent issues from different architectures line endings being different.
+	 * @return a string that should include the entire document
+	 * @throws IOException
+	 */
+	protected static String downloadRepo(Repository repo) throws IOException {
+		URL page = new URL(repo.listURL + "packages");
+		URLConnection connection = page.openConnection();
+		BufferedReader in = new BufferedReader( new InputStreamReader( connection.getInputStream()));
+		StringBuilder response = new StringBuilder(10000);
+		String inputLine;
+		//Read each line of repository list into a big String
+		while ((inputLine = in.readLine()) != null)
+			response.append(inputLine + "\n");
+		in.close();
+		return response.toString();
+	}
+
+	/**
+	 * Parses the response string from downloading a repository.
+	 * This splits each line out, and handles each one, splitting them each out into MCPackages 
+	 * @param page the raw repository which as downloaded
+	 * @param repo the repository object for which this pertains
+	 * @return the Vector which contains all the packages parsed from the html string
+	 * @throws IOException
+	 */
+	protected static Vector<MCPackage> parseUpdate(String page, Repository repo) throws IOException {
+		BufferedReader br = new BufferedReader(new StringReader(page));
+		String line;
+
+		MCPackage curPack = new MCPackage();
+		Vector<MCPackage> packs = new Vector<MCPackage>();
+
+		while ((line = br.readLine()) != null) {
+			switch (line) {
+			//Blank Line.  End of this package.  Load next.
+			case "":
+				System.out.println("Blank Line.  Loading next package");
+				packs.add(curPack);
+				curPack = new MCPackage();
+				break;
+			//Non-blank Line
+			default:
+				//Split all other lines into the tag (before the colon)
+				//and the content (after the colon and space)
+				String[] parts = line.split("\\:\\ ");
+				if (parts.length == 2)
+					dealLine(parts, curPack);
+				else
+					System.out.println("Most likely malformed repository:" + repo.listURL);
+				break;
+			}
+		}
+		//Save the last pack
+		packs.add(curPack);
+
+		return packs;
+	}
+
+	/**
+	 * Deals with a specific *normal* line in the repository
+	 * This is already confirmed to be a normal line, or this would not have been called 
+	 * @param line the string array which has been read. [0] should be the tag, [1] should be the data
+	 * @param pack the object which the resulting parsed data should be loaded into
+	 */
+	private static void dealLine(String[] line, MCPackage pack) {
+		switch (line[0]) {
+		case "Package":
+			pack.packageID = line[1];
+			break;
+		case "Version":
+			pack.version = line[1];
+			break;
+		case "Name":
+			pack.name = line[1];
+			break;
+		case "Shortdesc":
+			pack.shortDesc = line[1];
+			break;
+		case "Size":
+			pack.size = Integer.parseInt(line[1]);
+			break;
+		case "Tags":
+			dealTags(line[1]);
+			break;
+		case "Depends":
+			//TODO:  add depends parsing
+			break;
+		case "Suggests":
+			//TODO:  add suggests parsing
+			break;
+		case "Conflicts":
+			//TODO:  add conflicts parsing
+			break;
+		case "Replaces":
+			//TODO:  add replaces parsing
+			break;
+		case "MD5sum":
+			pack.md5Sum = line[1];
+			break;
+		case "Filename":
+			pack.fileName = line[1];
+			break;
+		case "Author":
+			pack.author = line[1];
+			break;
+		case "Maintainer":
+			pack.maintainer = line[1];
+			break;
+		case "Homepage":
+			pack.homePage = line[1];
+			break;
+		case "Description":
+			dealDescription(line[1]);
+			break;
+		default:
+			System.out.println("Unrecognized type:" + line[0]);
+		}
+	}
+
+	//TODO:  dealTags
+	private static void dealTags(String line) {
+		
+	}
+
+	//TODO:  dealDescription
+	private static void dealDescription(String line) {
+		
+	}
+}
--- src/core/Repository.java
+++ src/core/Repository.java
@@ -1,17 +1,23 @@
 package core;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.net.URL;
-import java.net.URLConnection;
+import java.util.Date;
 import java.util.Vector;
 
 public class Repository extends PackageList {
 
+	/**
+	 * The Raw URL of the root folder for the repository.
+	 * This SHOULD include the trailing slash
+	 */
 	public String listURL;
 
+	/**
+	 * A Repository.  This represents a discrete, public-facing file server
+	 * which serves mod packages and a Package list.
+	 * @param name the local name to use to differentiate the repository
+	 * @param url the URL to the root of the repository.  INCLUDE the trailing slash
+	 */
 	Repository (String name, String url) {
 		super(name);
 		this.listURL = url;
@@ -20,119 +26,18 @@ public class Repository extends PackageList {
 	//Since this is a repository, we fetch the repo, and update everything
 	public boolean updateList() {
 		try {
-			String html = downloadRepo();	//Fetch URL
-			Vector<MCPackage> list = parseUpdate(html);				//Parse into separate objects
+			String repoHtml = RepoParser.downloadRepo(this);			//Fetch URL
+			Vector<MCPackage> list = RepoParser.parseUpdate(repoHtml, this);	//Parse into separate objects
 			
-			//TODO:  if parsing was successful, replace all old objects
 			this.packageList = list;
-			//update lastUpdated
+			this.lastUpdated = new Date();
 			return true;
+
 		//TODO:  better exception handling & error reporting
 		} catch (IOException ioe) {
+			System.out.println("There was some sort of read error while updating:" + this.listName);
 			ioe.printStackTrace();
 			return false;
-		}
-	}
-
-	/**
-	 * Downloads the document for the repository, and then re-assembles the document, re-writing the line endings.
-	 * This should prevent issues from different architectures line endings being different.
-	 * @return a string that should include the entire document
-	 * @throws IOException
-	 */
-	private String downloadRepo() throws IOException {
-		URL page = new URL(this.listURL + "/lol.txt");  //TODO:  proper URL
-		URLConnection connection = page.openConnection();
-		BufferedReader in = new BufferedReader( new InputStreamReader( connection.getInputStream()));
-		StringBuilder response = new StringBuilder(5000);
-		String inputLine;
-		//Read each line of repository list into a big String
-		while ((inputLine = in.readLine()) != null)
-			response.append(inputLine + "\n");
-		in.close();
-		return response.toString();
-	}
-
-	private Vector<MCPackage> parseUpdate(String page) throws IOException {
-		BufferedReader br = new BufferedReader(new StringReader(page));
-		String line;
-
-		MCPackage curPack = new MCPackage();
-		Vector<MCPackage> packs = new Vector<MCPackage>();
-
-		while ((line = br.readLine()) != null) {
-			switch (line) {
-			//Blank Line.  End of this package.  Load next.
-			case "":
-				System.out.println("Blank Line.  Loading next package");
-				packs.add(curPack);
-				curPack = new MCPackage();
-				break;
-			//Non-blank Line
-			default:
-				String[] parts = line.split("\\:\\ ");
-				dealLine(parts, curPack);
-				break;
-			}
-		}
-		//Save the last pack
-		packs.add(curPack);
-
-		return packs;
-	}
-
-	private void dealLine(String[] line, MCPackage pack) {
-		switch (line[0]) {
-		case "Package":
-			pack.packageID = line[1];
-			break;
-		case "Version":
-			pack.version = line[1];
-			break;
-		case "Name":
-			pack.name = line[1];
-			break;
-		case "ShortDesc":
-			pack.shortDesc = line[1];
-			break;
-		case "Size":
-			pack.size = Integer.parseInt(line[1]);
-			break;
-		case "Tags":
-			//TODO:  add tag parsing
-			break;
-		case "Depends":
-			//TODO:  add depends parsing
-			break;
-		case "Suggests":
-			//TODO:  add suggests parsing
-			break;
-		case "Conflicts":
-			//TODO:  add conflicts parsing
-			break;
-		case "Replaces":
-			//TODO:  add replaces parsing
-			break;
-		case "md5Sum":
-			pack.md5Sum = line[1];
-			break;
-		case "Filename":
-			pack.fileName = line[1];
-			break;
-		case "Author":
-			pack.author = line[1];
-			break;
-		case "Maintainer":
-			pack.maintainer = line[1];
-			break;
-		case "Homepage":
-			pack.homePage = line[1];
-			break;
-		case "Description":
-			//TODO:  Implement Description handling
-			break;
-		default:
-			System.out.println("Unrecognized type:" + line[0]);
 		}
 	}
 
